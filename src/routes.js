5bd56c350d996c91e4415975f2ac8ce7702ea061
CelsoAlmeidaLF <celso.almeida.leite@hotmail.com>
2020-08-07 11:59:52 -0300

    projeto nodejs-api atualizado

--- src/routes.js
+++ src/routes.js
@@ -1,47 +1,86 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
+const sqlite3 = require('sqlite3');
+const dbname  = 'main.db';
 
 router.use(express.json());
 
-router.get("/", (req, res) => {
-
-    return res.json({ titulo: "api project" });
-
- });
-
-router.get("/view", (req, res) => {
-
-    return res.json({ titulo: "exibe lista" });
-
- });
-
+// POST => adiciona novos dados no banco de dados
+// data: 07/08/2020 rev.1.0
 router.post("/add", (req, res) => {
+    const { id, title, content } = req.body
+    let db = new sqlite3.Database(dbname, err => {
+      if (err){ throw err } console.log("Database start on: " + dbname) });
+    db.serialize(() => {
+      db.run('INSERT INTO blog(id, title, content) VALUES( $id, $title, $content )',{
+            $id:id,
+            $title:title,
+            $content:content
+          });
+      db.all('SELECT * FROM blog',(err, data) => {
+          if (err){ throw err } return res.json(data) });
+      db.close(err => { if (err){ throw err } console.log("closed: " + dbname) });
+    });
+});
 
-    return res.json({ titulo: "cadastrar dados" });
-
+// GET => retorna lista de dados
+// data: 07/08/2020 rev.1.0
+router.get("/view", (req, res) => {
+    let db = new sqlite3.Database(dbname, err => {
+      if (err){ throw err } console.log("Database start on: " + dbname) });
+    db.serialize(() => {
+        db.all('SELECT * FROM blog',(err, data) => {
+          if (err){ throw err } return res.json(data) });
+        db.close(err => { if (err){ throw err } console.log("closed: " + dbname) });
+    });
  });
 
+// GET/:id =. retorna item pelo seu id
+// data: 07/08/2020 rev.1.0
 router.get("/view/:id", (req, res) => {
+  const { id } = req.params
+  let db = new sqlite3.Database(dbname, err => {
+    if (err){ throw err } console.log("Database start on: " + dbname) });
+  db.serialize(() => {
+    db.all('SELECT * FROM blog WHERE id = ?',[id],(err, data) => {
+      if (err){ throw err } return res.json(data) });
+    db.close(err => { if (err){ throw err } console.log("closed: " + dbname) });
+  });
+ });
 
-    return res.json({ titulo: "lista item" });
-
-});
-
+// PUT/:id => edita item do banco de dados
+// data: 07/08/2020 rev.1.0
 router.put("/edit/:id", (req, res) => {
+   const { id } = req.params
+   const { title, content } = req.body
+   let db = new sqlite3.Database(dbname, err => {
+     if (err){ throw err } console.log("Database start on: " + dbname) });
+   db.serialize(() => {
+     db.run('UPDATE blog SET title = $title, content = $content  WHERE id = $id',{
+           $id:id,
+           $title:title,
+           $content:content
+         });
+     db.all('SELECT * FROM blog WHERE id = ?',[id],(err, data) => {
+         if (err){ throw err } return res.json(data) });
+     db.close(err => { if (err){ throw err } console.log("closed: " + dbname) });
+   });
+  });
 
-    return res.json({ titulo: "edita item" });
+// DELETE/:id => apaga item do banco de dados
+// data: 07/08/2020 rev.1.0
+router.delete("/delete/:id", (req, res) => {
+    const { id } = req.params
+    let db = new sqlite3.Database(dbname, err => {
+      if (err){ throw err } console.log("Database start on: " + dbname) });
+    db.serialize(() => {
+      db.run('DELETE FROM blog WHERE id = ?', id );
+      db.all('SELECT * FROM blog',(err, data) => {
+          if (err){ throw err } return res.json(data) });
+      db.close(err => { if (err){ throw err } console.log("closed: " + dbname) });
+    });
+   });
 
-});
-
-router.delete("/delete/:id", (req,res) => {
-
-    return res.json({ titulo: "deleta item" });
-
-});
-
-
-router.use(function(req, res, next) {
-  res.status(404).redirect('/')
-})
+// router.use(function(req, res, next) { res.status(404).redirect('/') })
 
 module.exports = router
